9a04ea3c25be8a4d31ac7651535cd208199e406a
Jeroen Venema <jeroen.venema@gmail.com>
2026-04-12 11:23:47 +0200

    Removed unused GPIO code, set release message

--- src/agon_os.s
+++ src/agon_os.s
@@ -1130,8 +1130,8 @@ STAR_EDIT:
 ; *VERSION
 ;
 STAR_VERSION:
-    CALL    TELL    ; Output the welcome message
-    .asciz      "BBC BASIC (eZ80) Version 5.00\n\rAGON eZ80/ADL release v0.1\n\r"
+    CALL        TELL    ; Output the welcome message
+    .asciz      "AGON eZ80/ADL release v0.1\n\r"
     RET
 
 ; OSCLI FX n
@@ -2135,109 +2135,6 @@ CSTR_CAT_1:
     INC         HL
     INC         DE
     JR          CSTR_CAT_1 ; Loop until finished
-
-;  A: Mode
-;  B: Pins
-;                  
-GPIOB_SETMODE:
-    CALL        SWITCH_A
-    .d24        GPIOB_M0 ; Output
-    .d24        GPIOB_M1 ; Input
-    .d24        GPIOB_M2 ; Open Drain IO
-    .d24        GPIOB_M3 ; Open Source IO
-    .d24        GPIOB_M4 ; Interrupt, Dual Edge
-    .d24        GPIOB_M5 ; Alt Function
-    .d24        GPIOB_M6 ; Interrupt, Active Low
-    .d24        GPIOB_M7 ; Interrupt, Active High
-    .d24        GPIOB_M8 ; Interrupt, Falling Edge
-    .d24        GPIOB_M9 ; Interrupt, Rising Edge
-
-; Output
-;
-GPIOB_M0:
-    RES_GPIO    PB_DDR,  B
-    RES_GPIO    PB_ALT1, B
-    RES_GPIO    PB_ALT2, B
-    RET
-
-; Input
-;
-GPIOB_M1:
-    SET_GPIO    PB_DDR,  B
-    RES_GPIO    PB_ALT1, B
-    RES_GPIO    PB_ALT2, B
-    RET
-
-; Open Drain IO
-;
-GPIOB_M2:
-    RES_GPIO    PB_DDR,  B
-    SET_GPIO    PB_ALT1, B
-    RES_GPIO    PB_ALT2, B
-    RET
-
-; Open Source IO
-;
-GPIOB_M3:
-    SET_GPIO    PB_DDR,  B
-    SET_GPIO    PB_ALT1, B
-    RES_GPIO    PB_ALT2, B
-    RET
-
-; Interrupt, Dual Edge
-;
-GPIOB_M4:
-    SET_GPIO    PB_DR,   B
-    RES_GPIO    PB_DDR,  B
-    RES_GPIO    PB_ALT1, B
-    RES_GPIO    PB_ALT2, B
-    RET
-
-; Alt Function
-;
-GPIOB_M5:
-    SET_GPIO    PB_DDR,  B
-    RES_GPIO    PB_ALT1, B
-    SET_GPIO    PB_ALT2, B
-    RET
-
-; Interrupt, Active Low
-;
-GPIOB_M6:
-    RES_GPIO    PB_DR,   B
-    RES_GPIO    PB_DDR,  B
-    SET_GPIO    PB_ALT1, B
-    SET_GPIO    PB_ALT2, B
-    RET
-
-
-; Interrupt, Active High
-;
-GPIOB_M7:
-    SET_GPIO    PB_DR,   B
-    RES_GPIO    PB_DDR,  B
-    SET_GPIO    PB_ALT1, B
-    SET_GPIO    PB_ALT2, B
-    RET
-
-
-; Interrupt, Falling Edge
-;
-GPIOB_M8:
-    RES_GPIO    PB_DR,   B
-    SET_GPIO    PB_DDR,  B
-    SET_GPIO    PB_ALT1, B
-    SET_GPIO    PB_ALT2, B
-    RET
-
-; Interrupt, Rising Edge
-;
-GPIOB_M9:
-    SET_GPIO    PB_DR,   B
-    SET_GPIO    PB_DDR,  B
-    SET_GPIO    PB_ALT1, B
-    SET_GPIO    PB_ALT2, B
-    RET
 
 ; Hook into the MOS VBLANK interrupt
 ;
