4d28ef943f93350a5b514054e8b4771eb26bd881
Jeroen Venema <jeroen.venema@gmail.com>
2026-04-18 14:18:00 +0200

    Release v0.3

--- src/agon_os.s
+++ src/agon_os.s
@@ -1142,7 +1142,7 @@ STAR_EDIT:
 ;
 STAR_VERSION:
     CALL        TELL    ; Output the welcome message
-    .asciz      "AGON eZ80/ADL release v0.2\n\r"
+    .asciz      "AGON eZ80/ADL release v0.3\n\r"
     RET
 
 ; OSCLI FX n
