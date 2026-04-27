176d1b139ab72330021916c41d1bc90079ff993d
Jeroen Venema <jeroen.venema@gmail.com>
2026-04-27 14:59:14 +0200

    Sync with upstream ez80 repo, MULT optimizations, PRs merged, minus reverted HIMEM stack change 36127c2

--- src/bbc-ez80_eval.s
+++ src/bbc-ez80_eval.s
@@ -4,29 +4,13 @@
     .global     COUNT1
     .global     INKEY1
     .global     EXPR_24BIT_INT
-    .global     INT24_TO_32
     .global     ITEMI24
     .global     TRUE
     .global     CONVERT_TO_I24
-    .global     DLOAD5_SPL
 
     .include    "equs.inc"
     .include    "macros.inc"
     .text
-
-; THIS FUNCTION REPLACES DLOAD5 FOR WHEN IX POINTS TO A STACK FRAME
-; CONTAINING 'PUSHED REGISTERS'.
-
-; AS REGISTERS ARE NOW 3 BYTES LONG THE OFFSETS FOR EACH BYTE NEEDS
-; TO BE ADJUSTED
-
-DLOAD5_SPL:
-	LD      B,(IX+6)
-	EXX
-	LD	DE, (IX+0)
-	EXX
-	LD	DE, (IX+3)
-	RET
 
 EXPR_24BIT_INT:
     CALL        EXPRI ; RESULT IN HLH'L' C SHOULD BE ZERO
@@ -51,34 +35,4 @@ ITEMI24:
     CALL        CONVERT_TO_I24
     RET
 
-; CONVERT UNSIGNED 24 BIT NUMBER IN HL
-; TO STANDARD 32BIT NUMBER REPRESENTATION
-; INPUT
-;   UHL = UNSIGNED 24 BIT NUMBER
-; OUTPUT
-;   HLH'L', C=0 = 32 BIT NUMBER
-;
-;INT24_TO_32:
-;    LD          (conversion_store), HL ; H'L' - LOW PART
-;    EXX
-;    LD          A, (conversion_store + 2)
-;    LD          L, A ; L = U
-;    XOR         A
-;    LD          H, A ; H = 0
-;    LD          C, A ; C = 0
-;    RET
-
-INT24_TO_32:
-    LD      (conversion_store),HL      ; store 24-bit address: low, mid, high
-
-    EXX
-    LD      HL,(conversion_store)      ; H'L' = low 16 bits (mid:low)
-    EXX
-
-    LD      HL,0
-    LD      A,(conversion_store+2)     ; high byte
-    LD      L,A                        ; HL = 00:high
-    XOR     A
-    LD      C,A                        ; integer marker
-    RET
     .include    "bbc-ez80/EVAL.Z80"
